9cbb4efee5c6cc352cdf70af2bd4d1e1ee6ce3a6
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-04-13 20:21:10 -0700

    Put fetchBreedDescription function to separate file and implement test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,16 @@
 const request = require("request");
 
-const searchTerm = process.argv.slice(2).join("");
-
-request(
-  `https://api.thecatapi.com/v1/breeds/search?q=${searchTerm}`,
-  function(error, response, body) {
-    if (response) {
-      const data = JSON.parse(body);
-      console.log(data[0].description);
-      if (data.length === 0) {
-        console.log("Breed not found.");
+const fetchBreedDescription = (searchTerm, callback) => {
+  request(
+    `https://api.thecatapi.com/v1/breeds/search?q=${searchTerm}`,
+    function (error, body) {
+      if (error) {
+        callback(error, null);
       }
+      const data = JSON.parse(body.body);
+      callback(null, data[0].description); // (error, desc)
     }
-    if (error) {
-      console.log(error.message);
-    }
-  }
-);
+  );
+};
+
+module.exports = { fetchBreedDescription };
